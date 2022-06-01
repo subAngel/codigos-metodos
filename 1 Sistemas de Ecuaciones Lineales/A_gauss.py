1c9466e82acea64bcb9dd7e839bea956bf910866
subAngel <whoangel.agl@gmail.com>
2022-05-31 23:11:40 -0500

    A) metodo de gauss

---
+++ 1 Sistemas de Ecuaciones Lineales/A_gauss.py
@@ -0,0 +1,8 @@
+# Metodo de Gauss
+# con ayuda de https://www.youtube.com/watch?v=qv9XxtNZg-U
+
+# Sistema de ecuaciones
+# 5x1 − x2 + 3x3 = 1
+# −x1 − 7x2 + x4 = −9
+# 2x2 + 8x3 − 2x4 = −4
+# 4x1 + x3 − 6x4 =
