01bf4663e538fdfc9ea340c8d7e5d853761fdab6
subAngel <whoangel.agl@gmail.com>
2022-06-12 01:36:46 -0500

    Ecuaciones Lineales - Determinantes

--- tutoriales/numpy-test.py
+++ tutoriales/numpy-test.py
@@ -1,14 +1,55 @@
 import numpy as np
-from numpy.linalg import solve, inv
 
-a = np.array([[-1.1,2.5],[1.3, 4.2]])    # Matriz forma (2,2)
-print(a)
-print(a.T)      # transpuesta
-print(inv(a))   # inversa
+A = np.array([[3., -0.1, -0.2],
+              [0.1, 7, -0.3],
+              [0.3, -0.2, 10]])
 
-b = np.array([[2],[-3]])    # vector forma (2,1)
-print(b)
-s = solve(a,2)  # solucionar sistema de ecuaciones
-print(s)
+B = np.array([7.85, -19.3, 71.4])
 
+X0 = np.array([0., 0., 0.])
 
+tolera = 0.00001
+iteramax = 100
+
+# PROCEDIMIENTO
+
+# Gauss-Seidel
+tamano = np.shape(A)
+n = tamano[0]
+m = tamano[1]
+#  valores iniciales
+X = np.copy(X0)
+diferencia = np.ones(n, dtype=float)
+errado = 2 * tolera
+
+itera = 0
+while not (errado <= tolera or itera > iteramax):
+    # por fila
+    for i in range(0, n, 1):
+        # por columna
+        suma = 0
+        for j in range(0, m, 1):
+            # excepto diagonal de A
+            if (i != j):
+                suma = suma - A[i, j] * X[j]
+
+        nuevo = (B[i] + suma) / A[i, i]
+        diferencia[i] = np.abs(nuevo - X[i])
+        X[i] = nuevo
+    errado = np.max(diferencia)
+    itera = itera + 1
+
+# Respuesta X en columna
+X = np.transpose([X])
+
+# revisa si NO converge
+if (itera > iteramax):
+    X = 0
+# revisa respuesta
+verifica = np.dot(A, X)
+
+# SALIDA
+print('respuesta X: ')
+print(X)
+print('verificar A.X=B: ')
+print(verifica)
